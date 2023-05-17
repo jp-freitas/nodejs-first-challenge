7e26bafb5f32e061345513fd533bf6ad37725bbe
jp-freitas <tec.joao.freitas@gmail.com>
2023-05-17 20:41:37 -0300

    Created update and select methods

--- src/database.js
+++ src/database.js
@@ -17,18 +17,6 @@ export class Database {
     fs.writeFile(databasePath, JSON.stringify(this.#database));
   }
 
-  // select(table, search) {
-  //   let data = this.#database[table] ?? [];
-  //   if (search) {
-  //     data = data.filter(row => {
-  //       return Object.entries(search).some(([key, value]) => {
-  //         return row[key].toLowerCase().includes(value.toLowerCase());
-  //       });
-  //     });
-  //   }
-  //   return data;
-  // }
-
   insert(table, data) {
     if (Array.isArray(this.#database[table])) {
       this.#database[table].push(data);
@@ -36,6 +24,27 @@ export class Database {
       this.#database[table] = [data]
     }
     this.#persist();
+    return data;
+  }
+
+  select(table, search) {
+    let data = this.#database[table] ?? [];
+    if (search) {
+      data = data.filter(row => {
+        return Object.entries(search).some(([key, value]) => {
+          return row[key].toLowerCase().includes(value.toLowerCase());
+        });
+      });
+    }
+    return data;
+  }
+
+  update(table, id, data) {
+    const rowIndex = this.#database[table].findIndex(row => row.id === id);
+    if (rowIndex > -1) {
+      this.#database[table][rowIndex] = { id, ...data };
+      this.#persist();
+    }
     return data;
   }
 
@@ -47,12 +56,5 @@ export class Database {
   //   }
   // }
 
-  // update(table, id, data) {
-  //   const rowIndex = this.#database[table].findIndex(row => row.id === id);
-  //   if (rowIndex > -1) {
-  //     this.#database[table][rowIndex] = { id, ...data };
-  //     this.#persist();
-  //   }
-  //   return data;
-  // }
+
 }
